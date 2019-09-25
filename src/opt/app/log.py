856e20d3eb9ae50fc554d7f3b40923b1e8e83613
Thomas Ngo Trung <T3tr0@users.noreply.github.com>
2019-09-25 11:11:33 +0200

    bug fix (#1)
    Adds exception handling for parsing timestamp in logging

--- src/opt/app/log.py
+++ src/opt/app/log.py
@@ -1,35 +1,34 @@
-import json
-import time
-
-import sys
-import os
 import datetime
-
+import json
+import os
+import sys
+import time
 import traceback
 
-
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 0)
 
 
 def info(message, timestamp=None, **kwargs):
-    print_for_stackdriver('info', message, timestamp, **kwargs)
+    print_for_stackdriver("info", message, timestamp, **kwargs)
 
 
 def debug(message, timestamp=None, **kwargs):
-    print_for_stackdriver('debug', message, timestamp, **kwargs)
+    print_for_stackdriver("debug", message, timestamp, **kwargs)
 
 
 def exception(ex, **kwargs):
-    print_for_stackdriver('error', 'Error: {}'.format(ex.message), **kwargs)
-    print_for_stackdriver('error', 'Traceback: {}'.format(traceback.format_exc()), **kwargs)
+    print_for_stackdriver("error", "Error: {}".format(ex.message), **kwargs)
+    print_for_stackdriver(
+        "error", "Traceback: {}".format(traceback.format_exc()), **kwargs
+    )
 
 
 def warn(message, timestamp=None, **kwargs):
-    print_for_stackdriver('warn', message, timestamp, **kwargs)
+    print_for_stackdriver("warn", message, timestamp, **kwargs)
 
 
 def error(message, timestamp=None, **kwargs):
-    print_for_stackdriver('error', message, timestamp, **kwargs)
+    print_for_stackdriver("error", message, timestamp, **kwargs)
 
 
 def log_handler(message):
@@ -38,19 +37,24 @@ def log_handler(message):
 
 def print_for_stackdriver(severity, message, timestamp=None, **kwargs):
 
-    if timestamp:
-        parsed_timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f+00:00")
-        time_seconds = (parsed_timestamp - datetime.datetime.utcfromtimestamp(0)).total_seconds()
-    else:
+    try:
+        parsed_timestamp = datetime.datetime.strptime(
+            timestamp, "%Y-%m-%dT%H:%M:%S.%f+00:00"
+        )
+
+        time_seconds = (
+            parsed_timestamp - datetime.datetime.utcfromtimestamp(0)
+        ).total_seconds()
+    except Exception:
         time_seconds = time.time()
 
     time_string = "%.9f" % time_seconds
 
     payload = {
-        'message': message.strip(),
-        'time': time_string,
-        'severity': severity,
-        'labels': kwargs
+        "message": message.strip(),
+        "time": time_string,
+        "severity": severity,
+        "labels": kwargs,
     }
 
-    print json.dumps(payload)
+    print(json.dumps(payload))
