4e188706a3a82fa6d7c9cf86464e8e6aed142d99
magnanc <cedric.magnan@artefact.com>
2020-02-26 14:47:16 +0100

    [FEATURE] Added SFTP subdirectory handling
    Added setstat, mkdir and rmdir permissions to the SFTP user
    
    Added default host port

--- src/opt/app/commands/upload_file.py
+++ src/opt/app/commands/upload_file.py
@@ -3,16 +3,18 @@ from loguru import logger
 import command
 import config
 from helpers import is_tmp
+import os
 
 
 def upload_file(file_path):
     buckets = config.APP_GCS_BUCKETS.split(",")
+    logger.info(f"Uploading file at path : {file_path}")
     for bucket in buckets:
         upload_command = [
             "/usr/bin/gsutil",
             "cp",
             file_path,
-            "gs://{}".format(bucket),
+            f"gs://{os.path.join(bucket, os.path.relpath(file_path, config.APP_LANDING_INGEST_DIR))}",
         ]
         if not is_tmp(file_path):
             command.run(upload_command, quiet=True)
